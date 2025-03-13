2dafcba047b611c9f0bf490d4cebc41eaa2acf53
atman-33 <gpbjk0304@gmail.com>
2025-03-13 09:26:38 +0900

    feat: update background color in AppTopLayout for improved design consistency

--- app/routes/_app._top/route.tsx
+++ app/routes/_app._top/route.tsx
@@ -4,7 +4,7 @@ import { AppNavLink } from './components/app-nav-link';
 const AppTopLayout = () => {
   return (
     <>
-      <div className="flex gap-12 rounded-md bg-gray-100 px-8">
+      <div className="flex gap-12 rounded-md bg-foreground/5 px-8">
         <AppNavLink to="/">Blog</AppNavLink>
         <AppNavLink to="/Apps">Apps</AppNavLink>
         <AppNavLink to="/Tags">Tags</AppNavLink>
