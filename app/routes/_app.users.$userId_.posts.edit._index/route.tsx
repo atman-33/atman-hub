270bac1c39cb42dd868864f62c78106c778e6146
atman-33 <gpbjk0304@gmail.com>
2025-03-31 09:39:20 +0900

    feat: enhance EditPostPage with form elements and layout

--- app/routes/_app.users.$userId_.posts.edit._index/route.tsx
+++ app/routes/_app.users.$userId_.posts.edit._index/route.tsx
@@ -1,8 +1,33 @@
-const EditPostPage = () => {
+import { Button } from '~/components/shadcn/ui/button';
+import { Input } from '~/components/shadcn/ui/input';
+import { Label } from '~/components/shadcn/ui/label';
+import { Textarea } from '~/components/shadcn/ui/textarea';
+
+export const EditPostPage = () => {
   return (
-    <>
-      <div>EditPostPage</div>
-    </>
+    <div className="flex flex-col">
+      <div className="flex justify-between p-4">
+        <Button variant="link">Back</Button>
+        <Button variant="default">Save Draft</Button>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
+          <Label htmlFor="emoji">Emoji:</Label>
+          <Input type="text" id="emoji" className="w-12" />
+        </div>
+        <div className="flex items-center gap-2">
+          <Label htmlFor="title">Title:</Label>
+          <Input type="text" id="title" />
+        </div>
+        <Button variant="default">Tags</Button>
+      </div>
+
+      <div className="flex flex-1 gap-4 pt-4">
+        <Textarea placeholder="Input" />
+        <Textarea placeholder="Preview" readOnly />
+      </div>
+    </div>
   );
 };
 
