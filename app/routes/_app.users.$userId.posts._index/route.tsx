72b8dfcc12365d7f0542b2b8e1d45ca998211802
atman-33 <gpbjk0304@gmail.com>
2025-04-02 09:02:09 +0900

    feat: replace Link with Form for creating new posts

--- app/routes/_app.users.$userId.posts._index/route.tsx
+++ app/routes/_app.users.$userId.posts._index/route.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router';
+import { Form } from 'react-router';
 import { Button } from '~/components/shadcn/ui/button';
 import { Separator } from '~/components/shadcn/ui/separator';
 
@@ -7,9 +7,11 @@ const UserPostsPage = () => {
     <div className="flex w-full flex-col gap-4">
       <div className="flex justify-between">
         <h2 className="font-bold text-2xl">📝Posts</h2>
-        <Link to="./edit">
-          <Button className="ml-4">New post</Button>
-        </Link>
+        <Form action="new" method="post">
+          <Button type="submit" name="_action" value="new" className="ml-4">
+            New post
+          </Button>
+        </Form>
       </div>
       <Separator />
     </div>
---
+++ app/routes/_app.users.$userId_.posts_.new/route.ts
@@ -0,0 +1,38 @@
+import { redirect } from 'react-router';
+import { prisma } from '~/.server/lib/prisma-client';
+import { commitSession, getSession } from '~/sessions.server';
+import type { Route } from './+types/route';
+
+export const action = async ({ request, params }: Route.ActionArgs) => {
+  const formData = await request.formData();
+  const { _action } = Object.fromEntries(formData);
+  console.log('_acton', _action);
+
+  switch (_action) {
+    case 'new': {
+      const res = await prisma.post.create({
+        data: {
+          title: 'draft',
+          emoji: '📝',
+          content: '',
+          author: { connect: { id: params.userId } },
+        },
+      });
+
+      const session = await getSession(request.headers.get('Cookie'));
+      session.flash('toast', {
+        type: 'success',
+        message: 'Post successfully created!',
+      });
+
+      const { userId } = params;
+      return redirect(`/users/${userId}/posts/${res.id}/edit`, {
+        headers: { 'Set-Cookie': await commitSession(session) },
+      });
+    }
+
+    default: {
+      console.log('_app.users.$userId_.posts_.new: _action not found');
+    }
+  }
+};
