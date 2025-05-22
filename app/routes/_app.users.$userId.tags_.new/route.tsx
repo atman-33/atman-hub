a81fb692a231c8f5a75b7c1c6d849dbc6cd696af
atman-33 <gpbjk0304@gmail.com>
2025-05-23 08:41:56 +0900

    feat: simplify tag creation by generating new TagID with UUID

--- app/routes/_app.users.$userId.tags_.new/route.tsx
+++ app/routes/_app.users.$userId.tags_.new/route.tsx
@@ -1,6 +1,4 @@
 import { redirect } from 'react-router';
-import { prisma } from '~/.server/lib/prisma-client';
-import { commitSession, getSession } from '~/sessions.server';
 import type { Route } from './+types/route';
 
 export const action = async ({ request, params }: Route.ActionArgs) => {
@@ -11,45 +9,11 @@ export const action = async ({ request, params }: Route.ActionArgs) => {
 
   switch (_action) {
     case 'new': {
-      // 既存の "New Tag %" に一致するタグを取得
-      const existingTags = await prisma.tag.findMany({
-        where: {
-          name: {
-            startsWith: 'New Tag',
-          },
-        },
-        select: { name: true },
-      });
-
-      // "New Tag N" の N を抽出
-      const usedNumbers = existingTags
-        .map((tag) => {
-          const match = tag.name.match(/^New Tag(?: (\d+))?$/);
-          return match ? Number(match[1] ?? 1) : null;
-        })
-        .filter((n): n is number => n !== null);
-
-      // 次の連番を決定
-      const nextNumber =
-        usedNumbers.length > 0 ? Math.max(...usedNumbers) + 1 : 1;
-      const newTagName = `New Tag ${nextNumber}`;
-
-      // 新規タグ作成
-      const res = await prisma.tag.create({
-        data: { name: newTagName },
-      });
-
-      // トーストメッセージ
-      const session = await getSession(request.headers.get('Cookie'));
-      session.flash('toast', {
-        type: 'success',
-        message: `"${newTagName}" successfully created!`,
-      });
+      // 新規作成用のTagIDをuuidで生成する
+      const newTagId = crypto.randomUUID();
 
       // 編集ページにリダイレクト
-      return redirect(`/users/${userId}/tags/${res.id}/edit`, {
-        headers: { 'Set-Cookie': await commitSession(session) },
-      });
+      return redirect(`/users/${userId}/tags/${newTagId}/edit`);
     }
   }
 };
