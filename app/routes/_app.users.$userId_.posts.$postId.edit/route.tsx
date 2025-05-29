b4f8a465b7605880ca222648418905670444c792
atman-33 <gpbjk0304@gmail.com>
2025-05-30 08:34:09 +0900

    feat: implement Markdown editor with preview and form handling

--- app/routes/_app.users.$userId_.posts.$postId.edit/route.tsx
+++ app/routes/_app.users.$userId_.posts.$postId.edit/route.tsx
@@ -2,12 +2,14 @@ import { getFormProps } from '@conform-to/react';
 import { parseWithZod } from '@conform-to/zod';
 import { useCallback, useEffect, useRef } from 'react';
 import { IoArrowBackCircle } from 'react-icons/io5';
-import { Link, redirect, useFetcher, useSubmit } from 'react-router';
+import { Link, Outlet, redirect, useFetcher, useSubmit } from 'react-router';
 import { prisma } from '~/.server/lib/prisma-client';
 import { showToast } from '~/components/shadcn/custom/custom-sonner';
 import { Button } from '~/components/shadcn/ui/button';
 import { Label } from '~/components/shadcn/ui/label';
+import { Sheet, SheetTrigger } from '~/components/shadcn/ui/sheet';
 import { Switch } from '~/components/shadcn/ui/switch';
+import { ButtonLink } from '~/components/shared/button-link';
 import { ConformInput } from '~/components/shared/conform/conform-input';
 import { commitSession, getSession } from '~/sessions.server';
 import type { Route } from './+types/route';
@@ -156,83 +158,89 @@ export const EditPostPage = ({
   });
 
   return (
-    <fetcher.Form
-      {...getFormProps(form)}
-      ref={formRef}
-      className="flex flex-col"
-    >
-      <div className="flex justify-between py-4">
-        {/* TODO: Buttonとnavigate(-1)を利用する方法に変更する */}
-        <Link to={`/users/${userId}/posts`}>
-          <IoArrowBackCircle className="h-10 w-10" />
-        </Link>
-        <div className="flex items-center gap-4">
-          <div className="flex items-center space-x-2">
-            <Switch id="publish" />
-            <Label htmlFor="publish">Publish</Label>
+    <Sheet>
+      <fetcher.Form
+        {...getFormProps(form)}
+        ref={formRef}
+        className="flex flex-col"
+      >
+        <div className="flex justify-between py-4">
+          {/* TODO: Buttonとnavigate(-1)を利用する方法に変更する */}
+          <Link to={`/users/${userId}/posts`}>
+            <IoArrowBackCircle className="h-10 w-10" />
+          </Link>
+          <div className="flex items-center gap-4">
+            <div className="flex items-center space-x-2">
+              <Switch id="publish" />
+              <Label htmlFor="publish">Publish</Label>
+            </div>
+            <SheetTrigger asChild>
+              <Button asChild>
+                <ButtonLink to="./tags" replace>
+                  Tags
+                </ButtonLink>
+              </Button>
+            </SheetTrigger>
+            {/* fetcher処理中（submitting/loading）は disable */}
+            <Button
+              variant="default"
+              type="submit"
+              disabled={
+                fetcher.state === 'submitting' || fetcher.state === 'loading'
+              }
+              onClick={(e) => {
+                e.preventDefault(); // NOTE: 手動フォーム送信させたいため、ブラウザによるフォーム送信を止める
+                save();
+              }}
+            >
+              Save Draft
+            </Button>
           </div>
-          {/* NOTE: <button type="button">は、ボタンがフォームを送信するのを防ぐHTMLの方法 */}
-          <Button variant="default" type="button">
-            Tags
-          </Button>
-          {/* fetcher処理中（submitting/loading）は disable */}
-          <Button
-            variant="default"
-            type="submit"
-            disabled={
-              fetcher.state === 'submitting' || fetcher.state === 'loading'
-            }
-            onClick={(e) => {
-              e.preventDefault(); // NOTE: 手動フォーム送信させたいため、ブラウザによるフォーム送信を止める
-              save();
-            }}
-          >
-            Save Draft
-          </Button>
         </div>
-      </div>
-      <div className="flex items-baseline gap-4">
-        <div className="flex items-baseline gap-2">
-          <Label htmlFor="emoji">Emoji:</Label>
-          <ConformInput
-            metadata={emoji}
-            aria-label="Emoji"
-            id="emoji"
-            name="emoji"
-            type="text"
-            defaultValue={post?.emoji}
-            className="w-12"
-          />
-        </div>
-        <div className="flex flex-1 items-baseline gap-2">
-          <Label htmlFor="title">Title:</Label>
-          <div className="flex-1">
+        <div className="flex items-baseline gap-4">
+          <div className="flex items-baseline gap-2">
+            <Label htmlFor="emoji">Emoji:</Label>
             <ConformInput
-              metadata={title}
-              aria-label="Title"
-              id="title"
-              name="title"
+              metadata={emoji}
+              aria-label="Emoji"
+              id="emoji"
+              name="emoji"
               type="text"
-              defaultValue={post?.title}
+              defaultValue={post?.emoji}
+              className="w-12"
             />
           </div>
+          <div className="flex flex-1 items-baseline gap-2">
+            <Label htmlFor="title">Title:</Label>
+            <div className="flex-1">
+              <ConformInput
+                metadata={title}
+                aria-label="Title"
+                id="title"
+                name="title"
+                type="text"
+                defaultValue={post?.title}
+              />
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className="flex flex-1 gap-4 pt-4">
-        <div className="flex-1">
-          {/* 記事エディター（div要素を利用しなくてはならない） */}
-          <div
-            aria-label="Content"
-            id="content"
-            ref={editor}
-            className="rounded-md border"
-          />
+        <div className="flex flex-1 gap-4 pt-4">
+          <div className="flex-1">
+            {/* 記事エディター（div要素を利用しなくてはならない） */}
+            <div
+              aria-label="Content"
+              id="content"
+              ref={editor}
+              className="rounded-md border"
+            />
+          </div>
+          {/* 記事プレビュー */}
+          <Preview className="flex-1" />
         </div>
-        {/* 記事プレビュー */}
-        <Preview className="flex-1" />
-      </div>
-    </fetcher.Form>
+      </fetcher.Form>
+      <Outlet />
+    </Sheet>
   );
 };
 
