3801b20fd1d02adac83ce8e73885d86bc18f801d
atman-33 <gpbjk0304@gmail.com>
2025-05-30 09:25:18 +0900

    feat: add onClick handler to Tag component for improved interactivity

--- app/components/shared/tag.tsx
+++ app/components/shared/tag.tsx
@@ -3,6 +3,7 @@ interface TagProps {
   imageUrl: string;
   size?: number;
   className?: string;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
 }
 
 /**
@@ -10,12 +11,28 @@ interface TagProps {
  * @param
  * @returns
  */
-export const Tag = ({ name, imageUrl, size, className }: TagProps) => {
+export const Tag = ({ name, imageUrl, size, className, onClick }: TagProps) => {
   const sizeClass = size ? `w-${size} h-${size}` : 'w-6 h-6';
+  // onClickがある場合はカーソルをpointerにする
+  const clickableClass = onClick ? 'cursor-pointer' : '';
 
   return (
     <div
-      className={`flex min-h-8 items-center space-x-1 rounded-4xl border-1 px-2 py-0.5 font-normal text-sm ${className}`}
+      className={`flex min-h-8 items-center space-x-1 rounded-4xl border-1 px-2 py-0.5 font-normal text-sm ${clickableClass} ${className}`}
+      onClick={onClick}
+      onKeyUp={
+        onClick
+          ? (e) => {
+              if (e.key === 'Enter' || e.key === ' ') {
+                onClick(
+                  e as unknown as React.MouseEvent<HTMLDivElement, MouseEvent>,
+                );
+              }
+            }
+          : undefined
+      }
+      tabIndex={onClick ? 0 : undefined}
+      role={onClick ? 'button' : undefined}
     >
       {imageUrl && (
         <img
