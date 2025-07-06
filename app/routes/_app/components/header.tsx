cfac3a733d8f0d7bcd8cc3c38d5930c684bed0fd
atman-33 <gpbjk0304@gmail.com>
2025-07-06 22:41:53 +0900

    fix: Update header logo image path and set explicit size

--- app/routes/_app/components/header.tsx
+++ app/routes/_app/components/header.tsx
@@ -6,9 +6,9 @@ const Header = () => {
     <header className="mb-4 flex items-center justify-between border-b pb-4">
       <Link to="/" className="flex items-center">
         <img
-          src="favicons/favicon-32x32.png"
+          src={`${import.meta.env.BASE_URL}favicons/android-chrome-192x192.png`}
           alt="TubeLoopPlayer logo"
-          className="mr-2"
+          className="mr-2 w-8 h-8"
         />
         <h1 className="font-semibold text-2xl">AtmanHub</h1>
       </Link>
